2d9aaae9d07d5e6fa1df5622538ae3016a7a6e64
DevBauke <boef.bosma@gmail.com>
2025-05-15 11:25:38 +0200

    Added Models

---
+++ ChapeauApp/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace ChapeauApp.Models
+{
+    public class Order
+    {
+        int OrderId { get; set; }
+        DateTime Time {  get; set; }
+        DateOnly Date {  get; set; }
+        string Status { get; set; }
+        int TableNumber { get; set; }
+        int EmployeeId { get; set; }
+
+    }
+}
